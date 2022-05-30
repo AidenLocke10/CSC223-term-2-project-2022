39e7622e911721c606d20a86c4baacc3552a0e71
Aiden Locke <lockeai@whs.school.nz>
2022-05-30 13:21:24 +1200

    the canvas now has a grid made from black lines

--- CSCproject2022.java
+++ CSCproject2022.java
@@ -7,23 +7,35 @@ import javax.swing.SwingUtilities;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
 /**
  * @author Aiden Locke
  * version 1 
  * start date (11/05/2022)
  */
- public class CSCproject2022 extends JPanel  {
-     
-     
-/**
-    * constructor for objects of class CSCproject2022
-    */
-   public CSCproject2022()
-   {
-      //initialise instance variables  
-       
-       
+public class CSCproject2022 extends JPanel  {
+
+    /**
+     * constructor for objects of class CSCproject2022
+     */
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        //g.setColor(Color.BLACK);
+        //g.drawLine(100,100,400,100);
+        int x1=0; //distance from the left side top of line 
+        int y1=0; //distance from top of canvas top of line 
+        int x2=400; // distance from left side bottom of line
+        int y2=400; // distance from top of canvas bottom of line
+
+        for(int x=0; x<400; x=x+20){ // loop for making horizantal lines
+            g.setColor(Color.BLACK); // setting color of line
+            g.drawLine(x,y1,x,y2); // drawing line
+        }
+
+        for(int y=0;y<400;y=y+20){ //loop for makinf vertical lines
+            g.setColor(Color.BLACK); 
+            g.drawLine(x1,y,x2,y);
+        }
     }
 
     /**
@@ -31,14 +43,15 @@ import javax.swing.JPanel;
      */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            var panel = new CSCproject2022();
-            panel.setBackground(Color.WHITE); //setting the cavas color
-            var frame = new JFrame("Conway's game of life");
-            frame.setSize(400, 400);  //setting the size of the canvas
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.getContentPane().add(panel, BorderLayout.CENTER);
-            frame.setVisible(true);
-           
-});
-}
+                var panel = new CSCproject2022();
+                panel.setBackground(Color.WHITE); //setting the cavas color
+                var frame = new JFrame("Conway's game of life");
+                frame.setSize(400, 400);  //setting the size of the canvas
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.getContentPane().add(panel, BorderLayout.CENTER);
+                frame.setVisible(true);
+
+            });
+
+    }
 }
